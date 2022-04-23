cfa27ea3d034eb960f1d0096e6f7796e0daa65c1
Yoshimasa Fuwa <little.ducks@icloud.com>
2022-04-23 19:34:59 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-    return <h1>Hello world</h1>;
+    return (
+        <>
+            <label htmlFor="bar">bar</label>
+            <input
+                type="text"
+                id="bar"
+                onClick={() => {
+                    console.log("click");
+                }}
+            ></input>
+        </>
+    );
 }
 
 export default App;
