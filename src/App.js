711e647d917e6c8cf18191bc29fb9299d709afc9
Yoshimasa Fuwa <little.ducks@icloud.com>
2022-04-24 10:29:41 +0900

    prop types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import PropTypes from "prop-types";
+
 const App = () => {
-    const profiles = [{ name: "Taro", age: 10 }, { name: "Hanako", age: 5 }, { name: "A" }];
+    const profiles = [
+        { name: "Taro", age: 10 },
+        { name: "Hanako", age: 5 },
+        { name: "A", age: 40 },
+    ];
 
     return (
         <>
@@ -18,8 +24,9 @@ const User = (props) => {
     );
 };
 
-User.defaultProps = {
-    age: 1,
+User.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired,
 };
 
 export default App;
